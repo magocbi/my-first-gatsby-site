6a25b3b8bf34f56e3b9b7988e2591d27eea94f38
Diego <newskyforge2@hotmail.com>
2022-03-15 18:04:16 -0600

    Add image for home page and plugins

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,4 @@
+import { StaticImage } from 'gatsby-plugin-image';
 import * as React from 'react';
 import Layout from '../components/Layout';
 
@@ -6,6 +7,10 @@ const IndexPage = () => {
   return (
     <Layout pageTitle='Home Page'>
       <p>I'm making this by following the Gatsby Tutorial</p>
+      <StaticImage
+        alt='Dog running on water at the beach'
+        src='../images/oscar-sutton-yihlaRCCvd4-unsplash.jpg'
+      />
     </Layout>
   );
 };
